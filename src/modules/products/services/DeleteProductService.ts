4b178597a35f85a49c43ff43ee1ed8ec460145bf
Stanislaw Cruz <stanycruz@outlook.com>
2021-12-19 15:21:24 -0300

    Serviço para Excluir um Produto

---
+++ src/modules/products/services/DeleteProductService.ts
@@ -0,0 +1,23 @@
+import { ProductRepository } from './../typeorm/repositories/ProductsRepository';
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+  id: string;
+}
+
+class DeleteProductService {
+  public async execute({ id }: IRequest): Promise<void> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found.');
+    }
+
+    await productsRepository.remove(product);
+  }
+}
+
+export default DeleteProductService;
