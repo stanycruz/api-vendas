1dcbb9c4fcc41e0fd1338fb3705403dd5e3d2efa
Stanislaw Cruz <stanycruz@outlook.com>
2021-12-19 11:10:26 -0300

    Serviço para Criação de Produto

--- src/shared/typeorm/migrations/1639852962320-CreateProduts.ts
+++ src/shared/typeorm/migrations/1639852962320-CreateProduts.ts
@@ -2,7 +2,7 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateProduts1639852962320 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid_ossp"');
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
     await queryRunner.createTable(
       new Table({
         name: 'products',
