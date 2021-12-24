2cb3d042d32585ba9fb561a9dc5e7ad64ba85048
Stanislaw Cruz <stanycruz@outlook.com>
2021-12-24 17:36:26 -0300

    Sessão de Autenticação com Token JWT

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -3,14 +3,20 @@ import { getCustomRepository } from 'typeorm';
 import AppError from '@shared/errors/AppError';
 import User from '../typeorm/entities/User';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 interface IRequest {
   email: string;
   password: string;
 }
 
+interface IResponse {
+  user: User;
+  token: string;
+}
+
 class CreateSessionsService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
     const user = await usersRepository.findByEmail(email);
 
@@ -24,7 +30,15 @@ class CreateSessionsService {
       throw new AppError('Incorrect email/password combination.', 401);
     }
 
-    return user;
+    const token = sign({}, 'fe76bac42b0d9350d9068c8cad3b5554', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
 
