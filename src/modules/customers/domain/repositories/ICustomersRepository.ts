24a390d37452b8263917f0c4710dba80c816e7a7
Stanislaw Cruz <stanycruz@outlook.com>
2022-01-29 22:10:32 -0300

    Camada de Infra - Refactoring TypeORM Repositories

--- src/modules/customers/domain/repositories/ICustomersRepository.ts
+++ src/modules/customers/domain/repositories/ICustomersRepository.ts
@@ -1,10 +1,10 @@
-// import { ICreateCustomer } from './../models/ICreateCustomer';
+import { ICreateCustomer } from './../models/ICreateCustomer';
 import { ICustomer } from '../models/ICustomer';
 
 export interface ICustomersRepository {
   findByName(name: string): Promise<ICustomer | undefined>;
   findById(id: string): Promise<ICustomer | undefined>;
   findByEmail(email: string): Promise<ICustomer | undefined>;
-  // create(data: ICreateCustomer): Promise<ICustomer>;
-  // save(customer: ICustomer): Promise<ICustomer>;
+  create(data: ICreateCustomer): Promise<ICustomer>;
+  save(customer: ICustomer): Promise<ICustomer>;
 }
