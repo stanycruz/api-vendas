c2c940399c0b669ec65ef2fef030372fe3750166
Stanislaw Cruz <stanycruz@outlook.com>
2022-02-11 19:21:12 -0300

    Ajustes nos controllers

--- src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -1,10 +1,11 @@
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 import { instanceToInstance } from 'class-transformer';
 import { Request, Response } from 'express';
-import UpdateUserAvatarService from '../../../services/UpdateUserAvatarService';
+import { container } from 'tsyringe';
 
 export default class UserAvatarController {
   public async update(request: Request, response: Response): Promise<Response> {
-    const updateAvatar = new UpdateUserAvatarService();
+    const updateAvatar = container.resolve(UpdateUserAvatarService);
 
     const user = updateAvatar.execute({
       user_id: request.user.id,
--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -1,11 +1,12 @@
+import CreateUserService from '@modules/users/services/CreateUserService';
+import ListUserService from '@modules/users/services/ListUserService';
 import { instanceToInstance } from 'class-transformer';
 import { Request, Response } from 'express';
-import CreateUserService from '../../../services/CreateUserService';
-import ListUserService from '../../../services/ListUserService';
+import { container } from 'tsyringe';
 
 export default class UsersController {
   public async index(request: Request, response: Response): Promise<Response> {
-    const listUser = new ListUserService();
+    const listUser = container.resolve(ListUserService);
 
     const users = await listUser.execute();
 
@@ -15,7 +16,7 @@ export default class UsersController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+    const createUser = container.resolve(CreateUserService);
 
     const user = await createUser.execute({
       name,
